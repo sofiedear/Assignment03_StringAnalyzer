8f601a9b68ba2b0eb9e533129c5ea71f03003dba
sofiedear <santonyuk2@mail.greenriver.edu>
2017-10-21 19:12:46 -0700

    Fixed - You can submit an empty message (Low)

--- StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/InputActivity.java
+++ StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/InputActivity.java
@@ -1,11 +1,12 @@
 package co.miniforge.corey.stringanalyzer;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class InputActivity extends AppCompatActivity {
     public static String intentTag = "inputData";
@@ -32,11 +33,19 @@ public class InputActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //App crashes when you hit submit (Critical)
-                //App crashed before arriving to AnalyzerActivity 
-                Intent intent = new Intent(getApplicationContext(), AnalyzerActivity.class);
-                intent.putExtra(intentTag, input.getText().toString());
-                startActivity(intent);
+
+                //Fixed - "You can submit an empty message (Low)"
+                if (input.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please enter a message",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //App crashes when you hit submit (Critical)
+                    //App crashed before arriving to AnalyzerActivity
+                    Intent intent = new Intent(getApplicationContext(), AnalyzerActivity.class);
+                    intent.putExtra(intentTag, input.getText().toString());
+                    startActivity(intent);
+                }
             }
         });
     }
